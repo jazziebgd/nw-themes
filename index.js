5a5060f8570c4bd478c88d671d8ce41ccebdd5f5
Dino Ivankov <dinoivankov@gmail.com>
2017-07-19 05:31:17 +0200

    Initial code commit

---
+++ index.js
@@ -0,0 +1,3 @@
+let themes = [];
+themes.push(require('./themes/dark-roboto/theme'));
+exports.themes = themes;
---
+++ themes/dark-roboto/theme.js
@@ -0,0 +1,16 @@
+exports.theme = {
+    path: __dirname,
+    name: 'dark-roboto',
+    initCssFiles: [
+        './css/config.css',
+        './css/fonts.css',
+    ],
+    cssFiles: [
+        './css/theme.css'
+    ],
+    overrideCssFiles: [
+        './css/style.css'
+    ],
+    initJsFiles: [],
+    jsFiles: []
+};
